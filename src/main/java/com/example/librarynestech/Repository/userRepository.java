18b3010aba46b37e74776fb98c4de4929dbe0331
Juicyman2805 <votranminhman@gmail.com>
2024-04-10 19:23:38 +0700

    new request

--- src/main/java/com/example/librarynestech/Repository/userRepository.java
+++ src/main/java/com/example/librarynestech/Repository/userRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface userRepository extends CrudRepository<User, Long> {
+public interface userRepository extends CrudRepository<User, String> {
 }
